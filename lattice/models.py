9a13fa5d1e1b9b6202b1930fb11bbe7332a73164
RossiAndrea <direlemming+github@gmail.com>
2013-11-27 11:32:49 +0100

    Extended the models file. Minor fixes
    Extended the models file with the `ThumbnailAbstract` that implements the
    upload of small images (800x800 by default).
    The function `crop_image`, binded on post-save signal of models that
    extends the `ThumbnailAbstract`, correctly resize and crop larger images
    to the given size.
    Stubs for `lattice/settings.py` (App sepcific settings) and `requirements.txt`

--- lattice/models.py
+++ lattice/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.conf import settings
 
 from django.contrib.sites.models import Site
+from django.db.models.signals import post_save
 from django.contrib.sites.managers import CurrentSiteManager
 
 from django.utils.translation import ugettext_lazy as _
 from django.template.defaultfilters import slugify
 
+import settings as app_settings
 import random
+from PIL import Image
 
 
 class SiteAbstract(models.Model):
@@ -17,14 +20,14 @@ class SiteAbstract(models.Model):
     If the `site id` is not given, the save method get the current site id.
     """
     objects = CurrentSiteManager()
-    site = models.ForeignKey("sites.Site", editable=False)
+    site = models.ForeignKey(Site, editable=False)
 
     class Meta:
         abstract = True
 
     def save(self, update_site=False, *args, **kwargs):
         if update_site or not self.id:
-            self.site_id = Site.objects.get_current()
+            self.site = Site.objects.get_current()
         super(SiteAbstract, self).save(*args, **kwargs)
 
     def is_displayable_for_site(self, site_id=None):
@@ -44,9 +47,12 @@ class SlugAbstract(models.Model):
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = self.__slugify(self.title)
+            self.slug = self.__slugify()
         super(SlugAbstract, self).save(*args, **kwargs)
 
     def __slugify(self):
@@ -63,7 +69,8 @@ class SlugAbstract(models.Model):
             except model.DoesNotExist:
                 # The slug is unique. Let's coninue
                 break
-            slug = slugify("%s-%s" % self.title, random.randrange(1111, 9999))
+            slug = slugify("%s-%s" % (self.title,
+                           random.randrange(1111, 9999)))
 
         return slug
 
@@ -79,11 +86,87 @@ class AuthorAbstract(models.Model):
     class Meta:
         abstract = True
 
-    def __str__(self):
-        return self.title
-
     def is_editable(self, request):
         """
         Restrict in-line editing to the objects's owner and superusers.
         """
         return request.user.is_superuser or request.user.id == self.user_id
+
+
+class ThumbnailAbstract(models.Model):
+    """
+    Abstract model that implements the upload of an image as thumbnail
+    """
+    thumbnail = models.ImageField(upload_to='img/thumbnails/%Y/%m/%d')
+
+    class Meta:
+        abstract = True
+
+
+def crop_image(sender, instance, created, **kwargs):
+    def _crop_image(path, size, crop_type='top'):
+        """
+        Resize and crop an image to fit the specified size.
+        args:
+            img_path: path for the image to resize.
+            modified_path: path to store the modified image.
+            size: `(width, height)` tuple.
+            crop_type: can be 'top', 'middle' or 'bottom', depending on this
+                value, the image will cropped getting the 'top/left', 'midle'
+                or 'bottom/rigth' of the image to fit the size.
+        raises:
+            Exception: if can not open the file in img_path of there is
+                problems in saving the image.
+            ValueError: if an invalid `crop_type` is provided.
+        """
+        # If height is higher we resize vertically, if not we resize
+        # horizontally
+        img = Image.open(path)
+        # Get current and desired ratio for the images
+        img_ratio = img.size[0] / float(img.size[1])
+        ratio = size[0] / float(size[1])
+        # The image is scaled/cropped vertically or horizontally depending on
+        # the ratio
+        if ratio > img_ratio:
+            img = img.resize((size[0], size[0] * img.size[1] / img.size[0]),
+                             Image.ANTIALIAS)
+            # Crop in the top, middle or bottom
+            if crop_type == 'top':
+                box = (0, 0, img.size[0], size[1])
+            elif crop_type == 'middle':
+                box = (0, (img.size[1] - size[1]) / 2, img.size[0],
+                       (img.size[1] + size[1]) / 2)
+            elif crop_type == 'bottom':
+                box = (0, img.size[1] - size[1], img.size[0], img.size[1])
+            else:
+                raise ValueError('ERROR: invalid value for crop_type')
+            img = img.crop(box)
+        elif ratio < img_ratio:
+            img = img.resize((size[1] * img.size[0] / img.size[1], size[1]),
+                             Image.ANTIALIAS)
+            # Crop in the top, middle or bottom
+            if crop_type == 'top':
+                box = (0, 0, size[0], img.size[1])
+            elif crop_type == 'middle':
+                box = ((img.size[0] - size[0]) / 2, 0, (img.size[0] + size[0])
+                       / 2, img.size[1])
+            elif crop_type == 'bottom':
+                box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
+            else:
+                raise ValueError('ERROR: invalid value for crop_type')
+            img = img.crop(box)
+        else:
+            img = img.resize((size[0], size[1]), Image.ANTIALIAS)
+            # If the scale is the same, we do not need to crop
+        img.save(path)
+
+    # Let's check if the sender model extends the ThumbnailAbstract model
+    if not issubclass(sender, ThumbnailAbstract):
+        return
+    _crop_image(instance.thumbnail.path,
+                app_settings.UPLOADED_IMG_SIZE,
+                'middle')
+
+# We don't specify the user model (because it's an abstract model). So we check
+# the name of it in the signal.
+post_save.connect(crop_image, dispatch_uid="image_auto_resize")
